033678c51499fa246f6c5d434850b363cec27d19
James Bland <jammydodger8888@gmail.com>
2026-04-14 20:08:35 +0100

    removed redundant console log

--- InitPingEllieAppApi.js
+++ InitPingEllieAppApi.js
@@ -1,5 +1,5 @@
 //send an initial ping to wake up the api
 //api hosted on free teir lol
 fetch("https://ellie-blog-app-api.onrender.com").then(() => {
-    console.log("pinged api")
+    //pinged api
 });
